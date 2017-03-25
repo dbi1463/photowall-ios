1401b94e65bde792cb0eba764f5add2637286152
Spirit <dbi1463@gmail.com>
2017-03-25 14:26:40 +0800

    Test Travis CI.

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'photowall' do
+    pod 'NitroKeychain'
+    pod 'AFNetworking'
+    pod 'MagicalRecord'
+
+    target 'photowallTests' do
+        pod 'Nocilla'
+        pod 'OCMock'
+    end
+end
---
+++ photowall/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  photowall
+//
+//  Created by Spirit on 3/25/17.
+//  Copyright © 2017 Picowork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
